739e9869b51dc861a57da697d49406b1394ade6d
jeffreycoen <jeff.coen@gmail.com>
2018-12-28 08:40:50 -0500

    added comments

--- ch08_racing.py
+++ ch08_racing.py
@@ -14,11 +14,14 @@ score = 0
 while True:
     x, y, z = accelerometer.get_values()
     print('x={}, y={}, z={}'.format(x, y, z))
+    
+    #  Plot the accelerometer reading for x-axis
+    print((x, ))
     sleep(10)
     track.set_pixel(randint(0, 4), 0, 5)
-    if x >=50 and car_position < 4:
+    if x >=25 and car_position < 4:
         car_position += 1
-    if x <=(-50) and car_position > 0:
+    if x <=(-25) and car_position > 0:
         car_position -= 1    
     if track.get_pixel(car_position, 4) > 0:
         display.scroll("GAME OVER!" + str(score))
